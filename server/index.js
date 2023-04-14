20ce9d58b2fb77f3ea73703c074707dadf609eb4
Jack Harmer <jack.harmer@simplyhealth.co.uk>
2023-04-14 16:29:50 +0100

    server display page updated

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ const passport = require("passport");
 const cookieSession = require("cookie-session");
 
 if (process.env.NODE_ENV !== "production") {
-    // Load environment variables from .env file in non prod environments
-    require("dotenv").config();
+  // Load environment variables from .env file in non prod environments
+  require("dotenv").config();
 }
 
 require("./strategies/JwtStrategy");
@@ -24,19 +24,19 @@ app.use(bodyParser.json());
 //Add the client URL to the CORS policy
 
 const whitelist = process.env.WHITELISTED_DOMAINS
-    ? process.env.WHITELISTED_DOMAINS.split(",")
-    : [];
+  ? process.env.WHITELISTED_DOMAINS.split(",")
+  : [];
 
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 
-    credentials: true,
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -47,16 +47,64 @@ app.use("/users", userRouter);
 app.use("/online", onlineRouter);
 
 app.get("/", async function (req, res) {
-    const user = await db.user.findUnique({
-        where: { username: "jack" },
-    });
-    res.send({ status: "success", userId: user.id, username: user.username });
+  res.type("html").send(html);
 });
 
 //Start the server in port 8081
 
 const server = app.listen(process.env.PORT || 8081, function () {
-    const port = server.address().port;
+  const port = server.address().port;
 
-    console.log("App started at port:", port);
+  console.log("App started at port:", port);
 });
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`;
